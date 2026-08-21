42bf2245bc764db186de04513f9f55be5c28015a
Nosewise0 <nonsaker021@gmail.com>
2026-08-21 14:21:37 +0800

    setting up fonts

--- front-end/src/pages/navbar.tsx
+++ front-end/src/pages/navbar.tsx
@@ -1,12 +1,5 @@
 export default function Navbar() {
     return (
-        <nav className="bg-black">
-            <ul>
-                <li>Home</li>
-                <li>Menu</li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+        <></>
     )
 }
